c089def674c8fa04dfca108d4585d905ed3b40e3
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-05-21 15:40:42 -0300

    Adiciona ligação com o banco de dados

--- src/main/java/com/fcamara/desafio/Model/Company.java
+++ src/main/java/com/fcamara/desafio/Model/Company.java
@@ -1,13 +1,28 @@
 package com.fcamara.desafio.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Companies")
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cnpj;
     private String address;
     private String phone;
-    private String carCapacity;
-    private String motorcycleCapacity;
+    private Integer carCapacity;
+    private Integer motorcycleCapacity;
+
+    public Company(String name, String cnpj, String address, String phone, Integer carCapacity, Integer motorcycleCapacity) {
+        this.name = name;
+        this.cnpj = cnpj;
+        this.address = address;
+        this.phone = phone;
+        this.carCapacity = carCapacity;
+        this.motorcycleCapacity = motorcycleCapacity;
+    }
 
     public String getName() {
         return name;
@@ -41,19 +56,19 @@ public class Company {
         this.phone = phone;
     }
 
-    public String getCarCapacity() {
+    public Integer getCarCapacity() {
         return carCapacity;
     }
 
-    public void setCarCapacity(String carCapacity) {
+    public void setCarCapacity(Integer carCapacity) {
         this.carCapacity = carCapacity;
     }
 
-    public String getMotorcycleCapacity() {
+    public Integer getMotorcycleCapacity() {
         return motorcycleCapacity;
     }
 
-    public void setMotorcycleCapacity(String motorcycleCapacity) {
+    public void setMotorcycleCapacity(Integer motorcycleCapacity) {
         this.motorcycleCapacity = motorcycleCapacity;
     }
 }
